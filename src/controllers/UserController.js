e376785623dbb3cc4ab495bd97ac5858b1e39d3a
Liam-M-Dev <liammassey21@gmail.com>
2023-07-26 18:04:55 +1000

    Scaffold user login function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,6 @@
 // Import User model, to create functionality with them
 const {User} = require("../models/UserModel");
-const { hashString, generateUserJWT } = require("../services/auth_services");
+const { hashString, generateUserJWT, validateHashedData } = require("../services/auth_services");
 
 
 
@@ -35,6 +35,31 @@ const createUser = async (request, response) => {
     }).catch((error) => {
         console.log(`Error occurred when saving user, error:\n` + error);
     })
+}
+
+// Function to log user in and return valid userJWT to client
+const loginUser = async (request, response) => {
+    try {
+        let savedUser = User.findOne({email: request.body.email})
+        if (!savedUser) {
+            response.status(400).json({message: "Email does not exist, please try again"})
+        }
+
+        let validPassword = await validateHashedData(request.body.password, savedUser.password);        
+        if ( !validPassword) {
+            response.status(400).json({message: "Emails do not match, try again"});
+        }
+
+        let encryptedToken = await generateUserJWT({
+            userID: savedUser.id,
+            username: savedUser.username,
+            email: savedUser.email 
+        })
+
+        response.json({message: "successful login", token: encryptedToken})
+    } catch (error) {
+        
+    }
 }
 
 
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -5,7 +5,11 @@ const {createUser} = require("../controllers/UserController");
 // Middleware Imports
 const { checkUserFields, checkValidEmail, checkPasswordLength } = require("../middleware/UserMiddleware");
 
+// Route for user signup
 router.post("/signup", checkUserFields, checkValidEmail, checkPasswordLength, createUser);
 
+
+// Route for user login
+router.post("/login", loginUser);
 
 module.exports = router;
