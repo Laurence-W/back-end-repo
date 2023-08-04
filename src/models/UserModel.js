55d178dabbd7b8c84e02c9c19fea8e0b8d11f426
Laurence-W <laurenceother@hotmail.com>
2023-08-04 22:00:02 +1000

    added completed runs field for users and updated seeds to have some completed and booked runs

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   bookings: [{ type: mongoose.Types.ObjectId, ref: "Event" }],
+  completedRuns: [{ type: mongoose.Types.ObjectId, ref: "Event" }],
   isAdmin: Boolean,
   isTrainer: Boolean,
 });
